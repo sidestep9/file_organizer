36cce4bd0578a2049201a4a46ef78a03207b85b2
sidestep9 <renhirtana@yahoo.com>
2026-07-05 11:52:07 +0700

    refactored mroe

--- main.py
+++ main.py
@@ -19,22 +19,22 @@ if not target_folder.exists():
     sys.exit()
 
 for file in target_folder.iterdir():
-    if not file.is_file:
+    if not file.is_file():
         continue
-    
-    for ext, folder in file_types.items():
-        if file.suffix.lower() == ext:
-            (target_folder / folder).mkdir(exist_ok=True)
-            numbering = 0
-            destination = (target_folder / folder / file.parts[-1])
 
-            while True:
-                if destination.exists():
-                    numbering += 1
-                    destination = (target_folder / folder / (file.stem + "(" + str(numbering) + ")" +"".join(file.suffixes)))
-                else:
-                    shutil.move(file, destination)
-                    break
+    folder = file_types.get(file.suffix.lower())
+    if folder is None:
+        continue
 
+    (target_folder / folder).mkdir(exist_ok=True)
+    numbering = 0
+    destination = (target_folder / folder / file.parts[-1])
+
+    while True:
+        if destination.exists():
+            numbering += 1
+            destination = (target_folder / folder / (file.stem + "(" + str(numbering) + ")" +"".join(file.suffixes)))
+        else:
+            shutil.move(file, destination)
             print(f"Moving: {str(file.parts[-1])} to {(target_folder / folder)}")
             break
