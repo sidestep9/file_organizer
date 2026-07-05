f7e9c3bb144d07adf0dd0e7d988852e9ba2a56ab
sidestep9 <renhirtana@yahoo.com>
2026-07-05 10:43:42 +0700

    added print to make it more readable

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import shutil
 
 file_types = {
     ".jpg": "IMAGES",
@@ -10,4 +11,6 @@ for file in target_folder.iterdir():
     for ext, folder in file_types.items():
         if str(file).endswith(ext):
             (target_folder / folder).mkdir(exist_ok=True)
+            shutil.move(file, (target_folder / folder))
+            print(f"Moving: {str(file.parts[-1])} to {(target_folder / folder)}")
             break
